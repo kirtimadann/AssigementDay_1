53948b9dd51ec14c87a713eb1722204c7b119054
lunainfotec <kirtimadan1997@gmail.com>
2023-03-21 12:19:51 +0530

    Part Time employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -15,3 +15,21 @@ else
 echo "Empoloyee is not present";
 
 fi
+
+
+ispresentt=1;
+randomcheckk="$((RANDOM%2))";
+
+if [ $ispresentt -eq $randomcheckk ];
+then
+       empratehr=20;
+        emphr=8;
+salary=$(($empratehr*$emphr));
+
+echo $salary;
+
+else
+
+salary=0;
+
+fi
